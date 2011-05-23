6405b5f139026a81977f8893c41f10686c66dd69
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2011-05-23 11:21:39 +0200

    moved rooms variable to a config.js file

---
+++ config.js
@@ -0,0 +1,10 @@
+var rooms = [
+  {id: "N26","name": "1-0-26"},
+  {id: "N30", "name": "1-0-30" , "class":"blue-col"},
+  {id: "N34", "name": "1-0-34" , "class":"yellow-col"},
+  {id: "N37", "name": "1-0-37"  , "class":"green-col"},
+  {id: "LUP1", "name": "Lille UP1"},
+  {id: "SUP1" ,"name": "Store UP1" , "class":"red-col"},
+  {id: "HARLEM","name": "Harlem", "class":""} 
+];
+
