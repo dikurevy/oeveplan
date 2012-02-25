c0cbf6d15a07be98cbaf2c8ffbf8f24e2497d9a4
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-02-25 15:57:44 +0100

    Made a helluva lot. Especially dragging handles and stuff

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var rooms = [
-  {id: "N26","name": "1-0-26"},
-  {id: "N30", "name": "1-0-30" , "class":"blue-col"},
-  {id: "N34", "name": "1-0-34" , "class":"yellow-col"},
-  {id: "N37", "name": "1-0-37"  , "class":"green-col"},
-  {id: "LUP1", "name": "Lille UP1"},
-  {id: "SUP1" ,"name": "Store UP1" , "class":"red-col"},
-  {id: "HARLEM","name": "Harlem", "class":""} 
+  {id: "N26","name": "Store UP1"},
+  {id: "N30", "name": "Store UP1" , "class":"blue-col"},
+//  {id: "N22", "name": "N022"},
+//  {id: "N24", "name": "N026"},
+  {id: "N34", "name": "N030" , "class":"yellow-col"},
+  {id: "N37", "name": "N034"  , "class":"green-col"},
+  {id: "LUP1", "name": "N037"},
+  {id: "SUP1" ,"name": "Lille UP1" , "class":"red-col"},
 ];
 
